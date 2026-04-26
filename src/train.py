5243f4d03e629fc2d5287b38684ab2a41dd9440d
Victoria Feng <vyf@duke.edu>
2026-04-26 17:29:04 -0400

    Training FINAL

--- src/train.py
+++ src/train.py
@@ -29,20 +29,20 @@ TOTAL_TIMESTEPS = 1_500_000
 ROLLOUT_STEPS = 4096      
 HIDDEN_DIM = 64
 LR = 3e-4            
-CLIP_EPS = 0.2
+CLIP_EPS = 0.4
 N_EPOCHS = 10           
 BATCH_SIZE = 64
 GAMMA = 0.99
 GAE_LAMBDA = 0.95
 # GAE_LAMBDA = 0.0        # 0.0 for ablation study
-ENTROPY_COEF = 0.01    # Decreased to encourage convergence
-# ENTROPY_COEF = 0.0     # 0.0 for ablation study
-VALUE_COEF = 0.5       # Decrease to prevent value loss from dominating policy updates
+# ENTROPY_COEF = 0.01    # Decreased to encourage convergence
+ENTROPY_COEF = 0.0     # 0.0 for ablation study
+VALUE_COEF = 0.2       # Decrease to prevent value loss from dominating policy updates
 MAX_GRAD_NORM = 0.5
 LOG_INTERVAL = 5         # Log every N updates
 SAVE_INTERVAL = 50        # Save checkpoint every N updates
 CHECKPOINT_PATH = "/content/drive/MyDrive/cs372_final_project/checkpoints"
-RUN_NAME = f"A2_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}_{datetime.now().strftime('%m%d%Y_%H%M%S')}"
+RUN_NAME = f"FINAL_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}_{datetime.now().strftime('%m%d%Y_%H%M%S')}"
 RESUME_CHECKPOINT = f"{CHECKPOINT_PATH}/ppo_update_400.pt"
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -52,7 +52,7 @@ print(f"Using device: {DEVICE}")
 torch.manual_seed(SEED)
 np.random.seed(SEED)
 
-env = gym.make(ENV_NAME)
+env = gym.make(ENV_NAME, reset_noise_scale=0.1)
 obs, _ = env.reset(seed=SEED)
 env.action_space.seed(SEED)
 
@@ -148,7 +148,7 @@ while timestep < TOTAL_TIMESTEPS:
 # Only for experiments
 torch.save(
     model.state_dict(),
-    f"{CHECKPOINT_PATH}/A2_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}.pt"
+    f"{CHECKPOINT_PATH}/FINAL_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}.pt"
 )
 print(f"Checkpoint saved at final update")
 
