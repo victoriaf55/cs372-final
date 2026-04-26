1997b64efa0117fdafb800aadca5d9eeea75d174
Victoria Feng <vyf@duke.edu>
2026-04-26 14:03:24 -0400

    training A3

--- src/train.py
+++ src/train.py
@@ -33,15 +33,16 @@ CLIP_EPS = 0.2
 N_EPOCHS = 10           
 BATCH_SIZE = 64
 GAMMA = 0.99
-GAE_LAMBDA = 0.95
-# ENTROPY_COEF = 0.01    # Decreased to encourage convergence
-ENTROPY_COEF = 0.0     # 0.0 for ablation study
+# GAE_LAMBDA = 0.95
+GAE_LAMBDA = 0.0        # 0.0 for ablation study
+ENTROPY_COEF = 0.01    # Decreased to encourage convergence
+# ENTROPY_COEF = 0.0     # 0.0 for ablation study
 VALUE_COEF = 0.5       # Decrease to prevent value loss from dominating policy updates
 MAX_GRAD_NORM = 0.5
 LOG_INTERVAL = 5         # Log every N updates
 SAVE_INTERVAL = 50        # Save checkpoint every N updates
 CHECKPOINT_PATH = "/content/drive/MyDrive/cs372_final_project/checkpoints"
-RUN_NAME = f"A1_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}_{datetime.now().strftime('%m%d%Y_%H%M%S')}"
+RUN_NAME = f"A3_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}_{datetime.now().strftime('%m%d%Y_%H%M%S')}"
 RESUME_CHECKPOINT = f"{CHECKPOINT_PATH}/ppo_update_400.pt"
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -147,7 +148,7 @@ while timestep < TOTAL_TIMESTEPS:
 # Only for experiments
 torch.save(
     model.state_dict(),
-    f"{CHECKPOINT_PATH}/A1_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}.pt"
+    f"{CHECKPOINT_PATH}/A3_study_lr{LR}_valcoeff{VALUE_COEF}_clip{CLIP_EPS}.pt"
 )
 print(f"Checkpoint saved at final update")
 
